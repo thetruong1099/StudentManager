5a34b347eb62bbaa77935072f88df95b56bc2bae
thetruong1099 <50746317+thetruong1099@users.noreply.github.com>
2021-03-31 10:37:21 +0700

    Initial commit

---
+++ app/src/main/java/com/example/studentmanager/Student.java
@@ -0,0 +1,51 @@
+package com.example.studentmanager;
+
+public class Student {
+    private int id;
+    private String name;
+    private String yearBirth;
+    private String numberPhone;
+    private String specialized;
+    private String type;
+
+    public Student() {
+    }
+
+    public Student(int id, String name, String yearBirth, String numberPhone, String specialized, String type) {
+        this.id = id;
+        this.name = name;
+        this.yearBirth = yearBirth;
+        this.numberPhone = numberPhone;
+        this.specialized = specialized;
+        this.type = type;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getYearBirth() {
+        return yearBirth;
+    }
+
+    public String getNumberPhone() {
+        return numberPhone;
+    }
+
+    public String getSpecialized() {
+        return specialized;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+}
